4e8c1bfe0a899afbb80a5dc8e11f9a29ecffdf8d
Trinity Fore <trinityfore@icloud.com>
2021-10-28 19:46:02 -0500

    quote data

--- script.js
+++ script.js
@@ -5,29 +5,28 @@ const BASE_URL = 'https://api.kanye.rest/';
 let apiData;
 
 //cached variables
-const $main = $('main');
 const $quote = $('#quote');
 
 
 // event listners
-$main.on('click', 'article', handleClick);
+//$main.on('click', handleClick);
 
 // functions
 function getData() {
     // get api data and assign it to our apiData state variable
-    $.ajax(BASE_URL, quote)
-    .then(function(quote) {
-        apiData = quote;
-        render();
-    }, function(error) {
-
-    });
-}
+    $.ajax(BASE_URL)
+    .then(
+        (data) => {
+            $quote.text(data.Quote);
+        }, (error) => {
+            console.log (error);
+        }
+        )}
 function handleClick(){
-    $
+    
 }
 
-function render() {
-    const kanyeFace = apiData 
+// function render() {
+//     const kanyeFace = apiData 
     
-};
+// };
